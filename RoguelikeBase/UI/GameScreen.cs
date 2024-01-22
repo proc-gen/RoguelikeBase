d94f3b742d72ffbea37cff0a823f16f33763a95e
Micah Caunter <mcaunter12@gmail.com>
2024-01-21 21:24:03 -0500

    feat: add game world class to hold items specific to the game running so it's separate from things needed directly by the game screen

--- RoguelikeBase/UI/GameScreen.cs
+++ RoguelikeBase/UI/GameScreen.cs
@@ -1,5 +1,8 @@
-﻿using RoguelikeBase.Constants;
+﻿using Arch.Core;
+using RoguelikeBase.Constants;
 using RoguelikeBase.Scenes;
+using RoguelikeBase.UI.Extensions;
+using RoguelikeBase.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,15 +18,31 @@ namespace RoguelikeBase.UI
         ScreenSurface screen;
         bool dirty = true;
 
+        GameWorld world;
+
         public GameScreen(RootScreen rootScreen)
         {
             RootScreen = rootScreen;
             screen = new ScreenSurface(GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT);
+            world = new GameWorld();
+
+            StartNewGame();
         }
+
+        private void StartNewGame()
+        {
+            world.GameLog.Add("Welcome traveler");
+            world.CurrentState = GameState.PlayerTurn;
+        }
+
 
         public override void Update(TimeSpan delta)
         {
-            HandleKeyboard();
+            if (world.CurrentState == GameState.PlayerTurn)
+            {
+                HandleKeyboard();
+            }
+
             RefreshScreen();
             base.Update(delta);
         }
@@ -42,9 +61,21 @@ namespace RoguelikeBase.UI
         {
             if(dirty)
             {
-                screen.Print(0, 0, "The Game Screen", Color.Yellow, Color.Black);
-                screen.Print(0, 1, "Press Esc to go back to the Main Menu", Color.Yellow, Color.Black);
+                DrawGameLog();
+
                 dirty = false;
+            }
+        }
+
+        private void DrawGameLog()
+        {
+            screen.DrawRLTKStyleBox(0, 39, 79, 10, Color.White, Color.Black);
+
+            int y = 40;
+            for (int i = 1; i <= Math.Min(9, world.GameLog.Count); i++)
+            {
+                screen.Print(2, y, world.GameLog[world.GameLog.Count - i]);
+                y++;
             }
         }
 
---
+++ RoguelikeBase/Utils/GameWorld.cs
@@ -0,0 +1,24 @@
+﻿using Arch.Core;
+using RoguelikeBase.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoguelikeBase.Utils
+{
+    internal class GameWorld
+    {
+        public World World { get; set; }
+        public GameState CurrentState { get; set; }
+        public List<string> GameLog { get; set; }
+
+        public GameWorld() 
+        {
+            World = World.Create();
+            CurrentState = GameState.Loading;
+            GameLog = new List<string>();
+        }
+    }
+}
