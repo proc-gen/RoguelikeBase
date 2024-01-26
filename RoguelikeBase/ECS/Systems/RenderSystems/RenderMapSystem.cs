0047e97e2374a6d582d6295d9b4cad930c22bd2d
Micah Caunter <mcaunter12@gmail.com>
2024-01-25 21:58:49 -0500

    feat: add field of view without exploration mode

--- RoguelikeBase/ECS/Systems/RenderSystems/RenderMapSystem.cs
+++ RoguelikeBase/ECS/Systems/RenderSystems/RenderMapSystem.cs
@@ -1,4 +1,5 @@
 ﻿using Arch.Core;
+using RoguelikeBase.Map;
 using RoguelikeBase.Utils;
 using System;
 using System.Collections.Generic;
@@ -18,12 +19,17 @@ namespace RoguelikeBase.ECS.Systems.RenderSystems
         public void Render(ScreenSurface screen)
         {
             var map = World.Maps[World.CurrentMap];
+            var playerFov = FieldOfView.CalculateFOV(World, World.PlayerRef);
+
             for (int i = 0; i < map.Width; i++)
             {
                 for (int j = 0; j < map.Height; j++)
                 {
-                    var tile = map.GetMapTile(i, j);
-                    screen.Surface[i, j].Background = tile.BackgroundColor;
+                    if (playerFov.Contains(new Point(i, j)))
+                    {
+                        var tile = map.GetMapTile(i, j);
+                        screen.Surface[i, j].Background = tile.BackgroundColor;
+                    }
                 }
             }
         }
---
+++ RoguelikeBase/Map/FieldOfView.cs
@@ -0,0 +1,107 @@
+﻿using Arch.Core;
+using Arch.Core.Extensions;
+using RoguelikeBase.ECS.Components;
+using RoguelikeBase.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoguelikeBase.Map
+{
+    public static class FieldOfView
+    {
+        public static List<Point> CalculateFOV(GameWorld World, EntityReference entity)
+        {
+            HashSet<Point> fov = new HashSet<Point>(192);
+
+            var entityPosition = entity.Entity.Get<Position>();
+            var map = World.Maps[World.CurrentMap];
+
+            List<Point> borderPoints = GetBorderPointsInSquare(map, entityPosition.Point, 7);            
+
+            foreach ( var borderPoint in borderPoints )
+            {
+                List<Point> pointsToBorder = GetPointsInLine(entityPosition.Point, borderPoint);
+
+                foreach(var point in pointsToBorder)
+                {
+                    if(Point.EuclideanDistanceMagnitude(entityPosition.Point, point) > 49)
+                    {
+                        break;
+                    }
+
+                    fov.Add(point);
+
+                    var tile = map.GetMapTile(point);
+                    if(tile.BaseTileType == Constants.BaseTileTypes.Wall)
+                    {
+                        break;
+                    }
+                }
+            }
+
+
+            return fov.ToList();
+        }
+
+        private static List<Point> GetBorderPointsInSquare(Map map, Point entityPosition, int range)
+        {
+            List<Point> borderCells = new List<Point>(range * 8);
+            for (int i = Math.Max(entityPosition.X - range, 0);
+                i < Math.Min(entityPosition.X + range + 1, map.Width);
+                i++)
+            {
+                for (int j = Math.Max(entityPosition.Y - range, 0);
+                    j < Math.Min(entityPosition.Y + range + 1, map.Height);
+                    j++)
+                {
+                    if (i == Math.Max(entityPosition.X - range, 0) ||
+                        j == Math.Max(entityPosition.Y - range, 0) ||
+                        i == Math.Min(entityPosition.X + range, map.Width - 1) ||
+                        j == Math.Min(entityPosition.Y + range, map.Height - 1))
+                    {
+                        borderCells.Add(new Point(i, j));
+                    }
+                }
+            }
+
+            return borderCells;
+        }
+
+        private static List<Point> GetPointsInLine(Point origin, Point destination)
+        {
+            List<Point> linePoints = new List<Point>() { origin };
+
+            int dx = Math.Abs(destination.X - origin.X);
+            int dy = Math.Abs(destination.Y - origin.Y);
+
+            Point sx = new Point(origin.X < destination.X ? 1 : -1, 0);
+            Point sy = (0, origin.Y < destination.Y ? 1 : -1);
+            int err = dx - dy;
+
+            Point current = origin;
+
+            do
+            {
+                int errorCheck = 2 * err;
+
+                if (errorCheck > -dy)
+                {
+                    err -= dy;
+                    current += sx;
+                }
+                if (errorCheck < dx)
+                {
+                    err += dx;
+                    current += sy;
+                }
+
+                linePoints.Add(current);
+            } while (current != destination);
+
+            return linePoints;
+        }
+    }
+}
