1230a089778f70b6e855acf6481f123bf7cb793f
Micah Caunter <mcaunter12@gmail.com>
2024-02-10 10:22:20 -0500

    feat: add more equipment components

---
+++ RoguelikeBase/ECS/Components/ArmorStats.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoguelikeBase.ECS.Components
+{
+    public struct ArmorStats
+    {
+        public int Armor { get; set; }
+    }
+}
