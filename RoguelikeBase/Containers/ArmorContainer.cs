3448991226b171f6c0e9bc67b7a92f67fd7898ca
Micah Caunter <mcaunter12@gmail.com>
2024-02-11 16:10:14 -0500

    feat: create container classes for weapons and armor

---
+++ RoguelikeBase/Containers/ArmorContainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoguelikeBase.Containers
+{
+    internal class ArmorContainer
+    {
+        public string Name { get; set; }
+        public int Armor { get; set; }
+        public char Glyph { get; set; }
+        public int R { get; set; }
+        public int G { get; set; }
+        public int B { get; set; }
+    }
+}
---
+++ RoguelikeBase/Containers/WeaponContainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoguelikeBase.Containers
+{
+    internal class WeaponContainer
+    {
+        public string Name { get; set; }
+        public int MinDamage { get; set; }
+        public int MaxDamage { get; set; }
+        public char Glyph { get; set; }
+        public int R { get; set; }
+        public int G { get; set; }
+        public int B { get; set; }
+    }
+}
